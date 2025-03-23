9e357035c73df0bddc994a33e344480ada3531e3
风意不平吹短歌 <1294538480@qq.com>
2025-03-23 12:35:16 +0800

    0001

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -1,26 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public Rigidbody rb;
-    public Collider co;
-    public float Speed;
+    //public Rigidbody rb;
+    //public Collider co;
+    //public float Speed;
+    //void Start()
+    //{
+    //    rb = GetComponent<Rigidbody>();
+    //    co = GetComponent<Collider>();
+    //}
+
+    //void FixedUpdate()
+    //{
+    //    PlayerMove();
+    //}
+
+    //void PlayerMove()
+    //{
+    //    float horizontalNum = Input.GetAxisRaw("Horizontal");
+    //    rb.velocity = new Vector2(horizontalNum*Speed,rb.velocity.y);
+    //}
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    private ObjectPreview preview;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        co = GetComponent<Collider>();
+    }
+
+    void OnEnable()
+    {
+        preview = new ObjectPreview();
     }
 
     void FixedUpdate()
     {
-        PlayerMove();
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //rb.velocity = new Vector3(moveHorizontal * moveSpeed,rb.velocity.y, moveVertical * moveSpeed);
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        rb.AddForce(movement * moveSpeed);
     }
 
-    void PlayerMove()
+    void OnDisable()
     {
-        float horizontalNum = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(horizontalNum*Speed,rb.velocity.y);
+        preview.Cleanup();
     }
 }
